1c85d443965e22b3ead4cc9db33d631e403abf7e
C.J. May <doctormay6@gmail.com>
2022-02-22 15:28:04 -0600

    replace firefox with torbrowser-launcher

--- startapp.sh
+++ startapp.sh
@@ -2,11 +2,11 @@
 
 # remove X1-lock if it exists
 [[ -f /tmp/.X1-lock ]] && rm /tmp/.X1-lock && echo "X1-lock found, deleting"
-# update to latest firefox version
-apt-get install --only-upgrade -y firefox
+# update to latest Tor browser launcher version
+apt-get install --only-upgrade -y torbrowser-launcher
 
-# start firefox and restart if closed
+# start Tor browser launcher and restart if closed
 while true
 do
-	/usr/bin/firefox $START_URL
+	/usr/bin/torbrowser-launcher
 done
